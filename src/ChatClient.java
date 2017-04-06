95e2d16d8e08c63f2b622954339860baec850250
DesiNF <a312567415@qq.com>
2017-04-06 19:45:05 +0800

    Chat0.2

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,19 +1,20 @@
 import java.awt.*;
 
-public class ChatClient extends Frame{
-	
-	TextField tfTxt=new TextField();
-	TextArea taContent=new TextArea();
+public class ChatClient extends Frame {
 
-	public static void main(String args[]){
+	TextField tfTxt = new TextField();
+	TextArea taContent = new TextArea();
+
+	public static void main(String args[]) {
 		new ChatClient().launchFrame();
 	}
-	
-	public void launchFrame(){
-		setLocation(400,300);
+
+	public void launchFrame() {
+		setLocation(400, 300);
 		this.setSize(300, 300);
-		add(taContent,BorderLayout.NORTH);
-		add(tfTxt,BorderLayout.SOUTH);
+		add(taContent, BorderLayout.NORTH);
+		add(tfTxt, BorderLayout.SOUTH);
+		pack();
 		this.setVisible(true);
 	}
 }

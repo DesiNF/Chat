0cad347a133d29aac4edc6f37d11cfb4eee9ab2a
DesiNF <a312567415@qq.com>
2017-04-06 20:42:47 +0800

    Chat0.4

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
+import java.net.*;
 
 public class ChatClient extends Frame {
 
@@ -25,8 +27,33 @@ public class ChatClient extends Frame {
 			}
 			
 		});
-		this.setVisible(true);
-	}
 
+		tfTxt.addActionListener(new TFListener());
+		this.setVisible(true);
+		connect();
+	}
+	
+	public void connect(){
+		try {
+			Socket s=new Socket("127.0.0.1",8888);
+System.out.println("succeed");
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	private class TFListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			String s=tfTxt.getText();
+			taContent.setText(s);
+			tfTxt.setText("");
+		}
+		
+	}
 
 }

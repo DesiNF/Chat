1fb5917a84a686d525ba542d732d1434d1eabc62
DesiNF <a312567415@qq.com>
2017-04-07 20:08:54 +0800

    Chat0.7 连接多个客户端

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -2,48 +2,88 @@ import java.io.*;
 import java.net.*;
 
 public class ChatServer {
+	boolean started = false;
+	ServerSocket ss = null;
 
 	public static void main(String args[]) {
-		boolean started = false;
-		ServerSocket ss=null;
-		DataInputStream dis=null;
-		Socket s =null;
-		
+		new ChatServer().start();
+	}
+
+	public void start() {
 		try {
 			ss = new ServerSocket(8888);
-		}catch(BindException e){
+			started = true;//端口连接成功后
+		} catch (BindException e) {
 			System.out.println("端口使用中");
 			System.out.println("请关闭相关程序");
 			System.exit(0);
-		}catch(IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		try{
-			started = true;
+		try {
+			//端口连接成功后处理客户端的连接
 			while (started) {
-				boolean bConnect = false;
-				s = ss.accept();
-				bConnect = true;
+				Socket s = ss.accept();
+				//客户端连接后new一个线程处理
+				Client c = new Client(s);
 				System.out.println("a client connected");
-				dis=new DataInputStream(s.getInputStream());
-				while (bConnect) {
-					String str = dis.readUTF();
-					System.out.println(str);
-				}
-				//dis.close();
+				new Thread(c).start();
 			}
-		}catch(EOFException e){
-			System.out.println("Client closed");
-		}catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
-			
-		}finally{
+		} finally {
 			try {
-				if(dis!=null)dis.close();
-				if(s!=null)s.close();
+				ss.close();
 			} catch (IOException e) {
+				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 	}
+
+	class Client implements Runnable {
+
+		private DataInputStream dis = null;
+		private Socket s = null;
+		private boolean bConnect = false;
+
+		public Client(Socket s) {
+			this.s = s;
+			try {
+				dis = new DataInputStream(s.getInputStream());
+				bConnect = true;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			try {
+				while (bConnect) {
+					String str = dis.readUTF();
+					System.out.println(str);
+				}
+			} catch (EOFException e) {
+				System.out.println("Client closed");
+			} catch (IOException e) {
+				e.printStackTrace();
+
+			} finally {
+				try {
+					if (dis != null)
+						dis.close();
+					if (s != null)
+						s.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+		}
+	}
+
 }
